e862fc279d945c18f52ad212d80cb0df0538a51a
andyvdoan <andyy.doan@gmail.com>
2021-02-09 13:33:44 -0600

    Created project boilerplate

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '5ed6b05e17c762c85f63674eda840e85'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '2dabd194-ba00-4120-8654-7542db801f0f'
+};
